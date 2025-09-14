29497519ddc32d26e4cad248676555cd7041a830
ih0027 <119897082+ih0027@users.noreply.github.com>
2025-09-14 19:43:50 -0400

    minimal test

--- script.js
+++ script.js
@@ -2,19 +2,17 @@ $('body').terminal(function(command, term) {
     const cmd = $.terminal.parse_command(command);
 
     if (cmd.name === 'play') {
-        play(term);  // from starwars-terminal.js
-    } else if (cmd.name === 'progress') {
-        runProgressBar(term, cmd.args[0]); // from progress_bar.js
+        term.echo("play")
     } else {
         term.echo(`Unknown command: ${cmd.name}`);
     }
 }, {
     prompt: '> ',
     greetings: 'Welcome to the combined terminal!',
-    keydown: function(e, term) {
-        if (isProgressRunning && e.ctrlKey && e.which === 68) {
-            cancelProgress(term); // from progress_bar.js
-            return false;
-        }
-    }
+//   keydown: function(e, term) {
+//        if (isProgressRunning && e.ctrlKey && e.which === 68) {
+//            cancelProgress(term); // from progress_bar.js
+//            return false;
+//        }
+//    }
 });
