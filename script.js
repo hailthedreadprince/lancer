580776a9bfc63c1b69569fb6d9102dda9f525690
ih0027 <119897082+ih0027@users.noreply.github.com>
2025-09-15 10:36:24 -0400

    Adding templates

--- script.js
+++ script.js
@@ -5,13 +5,15 @@ $('#terminal').terminal(function(command, term) {
         term.echo("play")
         term.echo(cmd.args)
     } else if (cmd.name === 'progress'){
-
+        var time = cmd.args[0]
+        var width = cmd.args[1]
+        
     } else {
         term.echo(`Unknown command: ${cmd.name}`);
     }
 }, {
     prompt: '> ',
-    greetings: 'Welcome to the combined terminal!',
+    greetings: startup.innerHTML,
     onInit: function(term) {
         //run after term initializes
         term.exec('startup potato potato')
