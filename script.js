929eaa75e0408771de26c96118efc028dd9d9788
ih0027 <119897082+ih0027@users.noreply.github.com>
2025-09-15 13:00:46 -0400

    Revert "Reapply "Refactored selectFrom into its own function""
    This reverts commit 13cbd0eebe350db9438a753405a2fd9625630464.

--- script.js
+++ script.js
@@ -1,37 +1,40 @@
 $('#terminal').terminal(function(command, term) {
     const cmd = $.terminal.parse_command(command);
-    var selected
+
     if (cmd.name === 'startup') {
         //TODO add startup animation
 
-        term.exec('selectFrom', {silent:true});
-        term.echo(selected);
-    } else if (cmd.name === 'progress'){
-        var time = cmd.args[0];
-        var width = cmd.args[1];
-        
-    } else if(cmd.name === 'selectFrom'){
-        for (i in cmd.args){
-            term.echo(i)
-        }
-        term.push(function(selection){
-            selection = parseInt(selection);
-            if (selection === NaN){
+        term.clear();
+        term.echo("Select Task");
+        term.echo("1. View operations");
+        term.echo("2. View remote connections");
+        term.echo("3. View diagnostics");
+        term.push(function(selection) {
+            if (selection === '1'){
+                term.echo("selected option 1");
+                term.pop();
+            } else if (selection === '2'){
+                term.echo("option 2");
+                term.pop();
+            } else if (selection ==='3'){
+                term.echo("Option 3");
+                term.pop();
+            } else {
                 term.clear();
-                term.echo("Invalid Selection")
-                for (i in cmd.args){term.echo(i)}
-            } else if (selection <= cmd.args.length-1 && selection >0){
-                selected = selection
-                term.pop()
-            }else{
-                term.clear()
-                term.echo("Invalid Selection")
-                for (i in cmd.args){term.echo(i)}
+                term.echo("Invalid Selection");
+                term.echo("Select Task");
+                term.echo("1. View operations");
+                term.echo("2. View remote connections");
+                term.echo("3. View diagnostics");
             }
         },{
             prompt: '>'
         })
-    }else {
+    } else if (cmd.name === 'progress'){
+        var time = cmd.args[0];
+        var width = cmd.args[1];
+        
+    } else {
         term.echo(`Unknown command: ${cmd.name}`);
     }
 }, {
