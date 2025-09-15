3ea88bb076e2643c9db0441c35fa85ab9e7f5b18
ih0027 <119897082+ih0027@users.noreply.github.com>
2025-09-15 11:12:31 -0400

    Added selector

--- script.js
+++ script.js
@@ -4,8 +4,26 @@ $('#terminal').terminal(function(command, term) {
     if (cmd.name === 'startup') {
         //TODO add startup animation
 
-        term.clear()
-        term.echo("Select Task /n 1. Potato /n 2. No")
+        term.clear();
+        term.echo("Select Task");
+        term.echo("1. View operations");
+        term.echo("2. View remote connections");
+        term.echo("3. View diagnostics");
+        term.push(function(selection) {
+            if (selection === '1'){
+                term.echo("selected option 1");
+                term.pop();
+            } else if (selection === '2'){
+                term.echo("option 2")
+                term.pop();
+            } else if (selection ==='3'){
+                term.echo("Option 3")
+                term.pop();
+            } else {
+                term.clear()
+                term.echo("Invalid Selection")
+            }
+        })
     } else if (cmd.name === 'progress'){
         var time = cmd.args[0]
         var width = cmd.args[1]
@@ -18,6 +36,6 @@ $('#terminal').terminal(function(command, term) {
     greetings: startup.innerHTML,
     onInit: function(term) {
         //run after term initializes
-        //term.exec('startup potato potato')
+        term.exec('startup potato potato')
     }
 });
