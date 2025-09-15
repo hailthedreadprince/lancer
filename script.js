8a15003cb9641a1c5443150d2c7b2bca043edad2
ih0027 <119897082+ih0027@users.noreply.github.com>
2025-09-15 11:00:34 -0400

    Started adding first menu

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ $('#terminal').terminal(function(command, term) {
     const cmd = $.terminal.parse_command(command);
 
     if (cmd.name === 'startup') {
-        term.echo("play")
-        term.echo(cmd.args)
+        //TODO add startup animation
+
+        term.clear()
+        term.echo("Select Task /n 1. Potato /n 2. No")
     } else if (cmd.name === 'progress'){
         var time = cmd.args[0]
         var width = cmd.args[1]
