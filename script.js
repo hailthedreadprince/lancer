523e6c1b32da3741ff26d06c82134f2b64ae45f9
ih0027 <119897082+ih0027@users.noreply.github.com>
2025-09-17 12:44:31 -0400

    Added operations first step and finished connections

--- script.js
+++ script.js
@@ -5,8 +5,24 @@ $('#terminal').terminal(function(command, term) {
         //TODO add startup animation
         term.exec("mainMenu", {silent:true})
     }else if (cmd.name === 'mainMenu'){
-        task = selectFromList(term, "Select Tast", ["1. View operations", "2. View remote connections", "3. View diagnostics"]);
+        task = selectFromList(term, "Select Task", ["1. View operations", "2. View remote connections", "3. View Mechs", "4. View diagnostics"]);
         term.echo(task);
+        if (task===1){
+            //View operations
+            op = selectFromList(term, "Select Operation", ["1. Operation Communism"])
+            if (op===1){
+                //Operation Communism
+
+            }else{term.echo("ERROR")}
+        }else if (task===2){
+            //Remote connections
+            connection=selectFromList(term, connectionsList.innerHTML, [`1  |Prison     |  ESTAB  | 04e8:dabf:f486:c83e:7dd0:3673:a969:390c |            Unknown            |
+`])
+        }else if (task===3){
+            //Mechs
+        }else if (task===4){
+            // Diagnostics
+        }else {term.echo("ERROR")}
     } else if (cmd.name === 'progress'){
         var time = cmd.args[0];
         var width = cmd.args[1];
